453228cf767ce0ab3486acb33c76da6535d69565
Алексей Андреев <andreev@htmlacademy.pro>
2024-05-29 03:48:37 +0400

    chartjs

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -22,7 +22,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <title>Профиль</title>
 </head>
