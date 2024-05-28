dab081da66ff7b450a4c23aa094be2f0c2c73952
Ilya Sofronov <ilya.sofron@mail.ru>
2024-05-29 03:17:32 +0400

    1

--- public/js/script.js
+++ public/js/script.js
@@ -582,6 +582,8 @@ class Chat {
 
         this.getNewMessages(false);
 
+        this.getMsgInterval = setInterval(this.getNewMessages, 5000)
+
         return this;
     }
 
@@ -592,6 +594,7 @@ class Chat {
     getMsgCountUri = '/apist/messages/count';
 
     getMsgCountInterval = null;
+    getMsgInterval = null;
 
     currentChatId = null;
 
