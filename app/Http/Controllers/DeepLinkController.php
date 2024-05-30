3ea13a8b7291ea0d881f2ff545ec449b780655bf
Алексей Андреев <andreev@htmlacademy.pro>
2024-05-30 18:29:24 +0400

    asd

--- app/Http/Controllers/DeepLinkController.php
+++ app/Http/Controllers/DeepLinkController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DeepLink;
 use Illuminate\Http\Request;
 
 class DeepLinkController extends Controller
 {
-    public function index() {
+    public function index($link) {
+        // $deepLink = DeepLink::where("link", $link)->first();
+        // if (!$deepLink) {
+        //     abort(404);
+        // }
+
+        // $stat = [
+        //     'link_id' => $deepLink->id,
+        //     'datatime' => date('Y-m-d'),
+        //     'device' => $deepLink->id,
+        //     'link_id' => $deepLink->id,
+
+        // ];
         echo "<pre>";
-        // print_r($_COOKIE);
-        print_r($_SERVER);
+        if ($link == 'browser') {
+            $browser = get_browser(null, true);
+            print_r($browser);
+        } else if ($link == 'link') {
+            echo "<a href='http://h406133820.nichost.ru/lnk/123'>123</a>";
+        } else {
+            phpinfo();
+        }
     }
 }
