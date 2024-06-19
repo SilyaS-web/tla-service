aa07a6b224787796a3a1c736420d5bc7f204b329
Алексей Андреев <andreev@htmlacademy.pro>
2024-06-20 02:51:35 +0400

    Иконки уведомления

--- app/Services/TgService.php
+++ app/Services/TgService.php
@@ -9,10 +9,39 @@ class TgService
 {
     public static function sendResetPassword($chat_id, $password)
     {
+        // $curl = curl_init();
+
+        // curl_setopt_array($curl, array(
+        //     CURLOPT_URL => '195.24.67.70:5000/reset-password',
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_ENCODING => '',
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 0,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => 'POST',
+        //     CURLOPT_POSTFIELDS => '{
+        //     "chatId": ' . $chat_id . ',
+        //     "newPassword": "' . $password . '"
+        // }',
+        //     CURLOPT_HTTPHEADER => array(
+        //         'Content-Type: application/json'
+        //     ),
+        // ));
+
+        // Log::info("chat_id " . $chat_id);
+        // Log::info("text " . $password);
+
+        // $response = curl_exec($curl);
+        // $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        // curl_close($curl);
+        // return $httpcode == 200 ? true : false;
+
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => '195.24.67.70:5000/reset-password',
+            CURLOPT_URL => '195.24.67.70:5000/notify',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -21,22 +50,19 @@ class TgService
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => '{
-            "chatId": ' . $chat_id . ',
-            "newPassword": "' . $password . '"
-        }',
+    "chatId": 6911168932,
+    "text": "Гандон"
+}',
             CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
+                'Content-Type: application/json',
+                'Authorization: Basic YXNleHByZXNzOk5ld0FjY2VzczIwMjM='
             ),
         ));
 
-        Log::info("chat_id " . $chat_id);
-        Log::info("text " . $password);
-
         $response = curl_exec($curl);
-        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
         curl_close($curl);
-        return $httpcode == 200 ? true : false;
+        echo $response;
     }
 
     public static function notify($chat_id, $text)
