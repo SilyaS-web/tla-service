c24386f18d0ccadfb989beba9764836798eac905
Алексей Андреев <andreev@htmlacademy.pro>
2024-06-29 20:17:13 +0400

    Заявка на блогера корректная работа кнопки

--- resources/views/blogger/card.blade.php
+++ resources/views/blogger/card.blade.php
@@ -110,7 +110,7 @@
                 <a href="{{ route('blogger-page', $blogger->id) }}" class="" style="color:rgba(0,0,0,.4); font-size:16px; font-weight:500; text-decoration:underline; margin-top: -20px;">Подробнее</a>
             </div>
             <div class="card__row card__row" style="flex-direction: column; gap: 5px">
-                <button class="btn btn-primary btn-add-to-project" onclick="sendProjectToBlogger({{ $blogger->id }})" data-project-id="">
+                <button class="btn btn-primary btn-add-to-project" onclick="sendProjectToBlogger({{ $blogger->id }}, this)" data-project-id="">
                     Отправить заявку
                 </button>
             </div>
--- resources/views/blogger/list.blade.php
+++ resources/views/blogger/list.blade.php
@@ -6,7 +6,7 @@
     @endforelse
 </div>
 <script>
-    function sendProjectToBlogger(user_id) {
+    function sendProjectToBlogger(user_id, el) {
         let p_id = $(document).find('.current-project').data('id') || null;
 
         if (!p_id) {
@@ -14,14 +14,28 @@
                 title: 'Ошибка', message: 'Выберите проект!'
             });
         } else {
-            $.post('/apist/works', {
-                blogger_id: user_id,
-                project_work_id: p_id
-            }, function(res) {
-                notify('info', {
-                    title: 'Успешно!', message: 'Заявка успешно отправлена!'
-                });
-            })
+            el.innerHTML = '<div class="lds-dual-ring"></div>';
+            $.post({
+                url: '/apist/works',
+                data: {
+                    blogger_id: user_id,
+                    project_work_id: p_id
+                },
+                success: function(data, textStatus, jqXHR) {
+                    el.innerHTML = 'Заявка отправлена';
+                    el.disabled = true;
+                    notify('info', {
+                        title: 'Успешно!', message: 'Заявка успешно отправлена!'
+                    });
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    el.innerHTML = 'Отправить заявку';
+                    notify('error', {
+                        title: 'Ошибка!',
+                        message: 'Не удалось отправить заявку!'
+                    });
+                }
+            });
         }
     }
 </script>
