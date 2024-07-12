c511744e304e3d3f13cb27c75888fb7a3d5303a2
Алексей Андреев <andreev@htmlacademy.pro>
2024-07-12 15:01:00 +0400

    ТЕкст пустой переписки

--- resources/views/shared/chat/chat-list.blade.php
+++ resources/views/shared/chat/chat-list.blade.php
@@ -2,6 +2,6 @@
     @forelse ($works as $work)
     @include('shared.chat.item')
     @empty
-    <p class="chat__chat-empty">Переписка пустая, <span style="color:var(--primary); cursor:pointer" onclick="(function(){ $(document).find('.nav-menu__item.project-link').click() })();">создайте проект</span> и начните работу с блогерами</p>
+    <p class="chat__chat-empty">Сообщений нет, <span style="color:var(--primary); cursor:pointer" onclick="(function(){ $(document).find('.nav-menu__item.project-link').click() })();">создайте проект</span> и начните работу с блогерами</p>
     @endforelse
 </div>
