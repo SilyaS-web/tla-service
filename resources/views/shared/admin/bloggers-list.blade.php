e000e76d11d136c32dea7980896a8e26cea7fe66
Ilya Sofronov <ilya.sofron@mail.ru>
2024-08-11 21:44:05 +0400

    поправил внешний вид, поправил форму модерации блогера

--- resources/views/shared/admin/bloggers-list.blade.php
+++ resources/views/shared/admin/bloggers-list.blade.php
@@ -1,4 +1,4 @@
-<div class="blogers-list__list list-blogers">
+<div class="blogers-list__list list-blogers admin-view__content-wrap">
     @forelse ($bloggers as $blogger)
     @include('shared.admin.blogger-card')
     @empty
--- resources/views/shared/admin/sellers-list.blade.php
+++ resources/views/shared/admin/sellers-list.blade.php
@@ -1,4 +1,4 @@
-<div class="blogers-list__list list-blogers">
+<div class="blogers-list__list list-blogers admin-view__content-wrap">
     @forelse ($sellers as $seller)
     @include('shared.admin.seller-card')
     @empty
