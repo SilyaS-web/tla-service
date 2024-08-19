f49aa96cbe60923ec7fa3df8fea7d8021a3aba6f
Алексей Андреев <andreev@htmlacademy.pro>
2024-08-19 15:32:15 +0400

    Фикс удаления проекта

--- resources/views/shared/chat/item.blade.php
+++ resources/views/shared/chat/item.blade.php
@@ -9,7 +9,7 @@
             <span>{{ isset($work->project) ? $work->project->product_name : 'Проект удалён' }}</span>
             <p>ID: {{$work->id}}</p>
         </div>
-        <a href="#" class="item-chat__project-link {{ auth()->user()->role == 'seller' ? "item-chat__project-link--seller" : 'item-chat__project-link--blogger' }}" data-project-id = "{{$work->project->id}}">Перейти к проекту</a>
+        <a href="#" class="item-chat__project-link {{ auth()->user()->role == 'seller' ? "item-chat__project-link--seller" : 'item-chat__project-link--blogger' }}" data-project-id = "{{$work->project->id ?? 0}}">Перейти к проекту</a>
     </div>
     @php($new_messages_count = $work->messages()->where('viewed_at', null)->where('user_id', '<>', auth()->user()->id)->count())
     @if ($new_messages_count > 0)
