9cc1a40da6bedb56dd80bba0950cd8d66dc8f0ff
Алексей Андреев <andreev@htmlacademy.pro>
2024-08-21 01:15:25 +0400

    Ресурс платежей

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -16,7 +16,7 @@ class PaymentResource extends JsonResource
         return [
             'id' => $this->id,
             'payment_id' => $this->payment_id,
-            'tariff' => $this->tariff->title , ' - ' . $this->tariff->tariffGroup->title,
+            'tariff' => $this->tariff->title . ' - ' . $this->tariff->tariffGroup->title,
             'created_at' => date_format($this->created_at, 'd.m.y'),
             'price' => $this->price / 100,
             'status' => $this->status,
