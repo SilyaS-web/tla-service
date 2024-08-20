139db11fe847aaeed2c2acd914ed0a13818bdb4a
Алексей Андреев <andreev@htmlacademy.pro>
2024-08-20 23:43:06 +0400

    Фикс миграций

--- database/migrations/2024_08_20_211428_create_platforms_table.php
+++ database/migrations/2024_08_20_211428_create_platforms_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlatforsTable extends Migration
+class CreatePlatformsTable extends Migration
 {
     /**
      * Run the migrations.
