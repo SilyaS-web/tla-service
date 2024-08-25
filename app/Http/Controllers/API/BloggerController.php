651e867aee5034e549d0df468983828cb27f7779
Алексей Андреев <andreev@htmlacademy.pro>
2024-08-25 21:20:35 +0400

    Принятие API

--- app/Http/Controllers/API/BloggerController.php
+++ app/Http/Controllers/API/BloggerController.php
@@ -99,6 +99,10 @@ class BloggerController extends Controller
         ]);
 
         foreach ($validated['platforms'] as $blogger_platform) {
+            if (!empty($blogger_platform)) {
+                continue;
+            }
+            
             $platform = $blogger->platforms()->where('platform_id', $blogger_platform->id)->first();
             if (isset($platform['link']) && !empty($platform['link'])) {
                 if ($platform) {
