93aace92f57b39d9c813d29510cbdc6073e9de89
Алексей Андреев <andreev@htmlacademy.pro>
2024-08-29 23:04:47 +0400

    Бэкэнд для рефорк

--- database/migrations/2024_08_29_213110_create_referral_codes_table.php
+++ database/migrations/2024_08_29_213110_create_referral_codes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReferralLinksTable extends Migration
+class CreateReferralCodesTable extends Migration
 {
     /**
      * Run the migrations.
