13d5298fe156f67c356b53bb67b3c5bf28e3d5ef
Ilya Sofronov <ilya.sofron@mail.ru>
2024-08-29 21:49:15 +0400

    компоненты авторизации и регистрации

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,16 +11,28 @@ import AdminSellersPage from './components/admin/pages/AppSellers.vue'
 import AdminProjectsPage from './components/admin/pages/AppProjects.vue'
 import AdminOrdersPage from './components/admin/pages/AppOrders.vue'
 
+const adminApp = createApp();
+
+adminApp.component('admin-index', AdminIndex)
+adminApp.component('admin-header', AdminHeader)
+adminApp.component('admin-aside', AdminAside)
+adminApp.component('admin-bloggers-moderation-page', AdminModerationPage)
+adminApp.component('admin-bloggers-page', AdminBloggersPage)
+adminApp.component('admin-sellers-page', AdminSellersPage)
+adminApp.component('admin-projects-page', AdminProjectsPage)
+adminApp.component('admin-orders-page', AdminOrdersPage)
+
+
+adminApp.mount('#admin-app')
+
+
+import Auth from './components/auth/pages/AppAuth.vue'
+import Register from './components/auth/pages/AppRegister.vue'
+
 const app = createApp();
 
-app.component('admin-index', AdminIndex)
-app.component('admin-header', AdminHeader)
-app.component('admin-aside', AdminAside)
-app.component('admin-bloggers-moderation-page', AdminModerationPage)
-app.component('admin-bloggers-page', AdminBloggersPage)
-app.component('admin-sellers-page', AdminSellersPage)
-app.component('admin-projects-page', AdminProjectsPage)
-app.component('admin-orders-page', AdminOrdersPage)
+app.component('auth-page', Auth)
+app.component('register-page', Register)
 
 
-app.mount('#admin-app')
+app.mount('#app')
---
+++ resources/js/components/services/api/User.vue
@@ -0,0 +1,49 @@
+<script>
+    import axios from 'axios'
+
+    const User = {
+        auth: (data) => {
+            return new Promise((resolve, reject) => {
+                axios({
+                    method: 'post',
+                    url: '/api/users/login/',
+                    data: data
+                })
+                .then((response) => {
+                    resolve(response.data)
+                })
+                .catch((errors) => {
+                    notify('error', {
+                        title: 'Внимание!',
+                        message: 'Невозможно авторизоваться. Проверьте все поля, если все в порядке, напишите в поддержку.'
+                    });
+
+                    resolve(response.data)
+                })
+            })
+        },
+
+        register: (data) => {
+            return new Promise((resolve, reject) => {
+                axios({
+                    method: 'post',
+                    url: '/api/users/register/',
+                    data: data
+                })
+                .then((response) => {
+                    resolve(response.data)
+                })
+                .catch((errors) => {
+                    notify('error', {
+                        title: 'Внимание!',
+                        message: 'Невозможно зарегистрироваться. Проверьте все поля, если все в порядке, напишите в поддержку.'
+                    });
+
+                    resolve(response.data)
+                })
+            })
+        }
+    };
+
+    export default User
+</script>
