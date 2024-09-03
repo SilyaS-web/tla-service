be341a1ad02e13d105185af3372cd02c4abee5e6
Алексей Андреев <andreev@htmlacademy.pro>
2024-09-03 20:44:29 +0400

    Sunctum middleware

--- routes/api.php
+++ routes/api.php
@@ -26,32 +26,35 @@ Route::post('/login', [AuthController::class, 'authenticate']);
 Route::get('/users/reset-password', [AuthController::class, 'resetPassword']);
 Route::get('/users/phone-confirmed', [AuthController::class, 'isTgConfirmed']);
 
-Route::post('/users', [AuthController::class, 'store']);
-Route::delete('/users/{user}', [UserController::class, 'delete']);
-Route::get('/users/{user}/dashboard', [DashboardController::class, 'index']);
-Route::get('/users/{user}/ban', [UserController::class, 'ban']);
-Route::get('/users/{user}/unban', [UserController::class, 'unban']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/users', [AuthController::class, 'store']);
+    Route::delete('/users/{user}', [UserController::class, 'delete']);
+    Route::get('/users/{user}/dashboard', [DashboardController::class, 'index']);
+    Route::get('/users/{user}/ban', [UserController::class, 'ban']);
+    Route::get('/users/{user}/unban', [UserController::class, 'unban']);
 
-Route::get('/bloggers', [BloggerController::class, 'index']);
-Route::get('/bloggers/{blogger}', [BloggerController::class, 'show']);
-Route::post('/bloggers/{blogger}/accept', [BloggerController::class, 'accept']);
+    Route::get('/bloggers', [BloggerController::class, 'index']);
+    Route::get('/bloggers/{blogger}', [BloggerController::class, 'show']);
+    Route::post('/bloggers/{blogger}/accept', [BloggerController::class, 'accept']);
 
-Route::get('/sellers', [SellerController::class, 'index']);
+    Route::get('/sellers', [SellerController::class, 'index']);
 
-Route::get('/projects', [ProjectController::class, 'index']);
-Route::post('/projects', [ProjectController::class, 'store']);
-Route::get('/projects/{project}', [ProjectController::class, 'show']);
-Route::get('/projects/{project}/ban', [ProjectController::class, 'ban']);
-Route::get('/projects/{project}/unban', [ProjectController::class, 'unban']);
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    Route::get('/projects/{project}/ban', [ProjectController::class, 'ban']);
+    Route::get('/projects/{project}/unban', [ProjectController::class, 'unban']);
 
-Route::post('/works', [WorkController::class, 'store']);
-Route::get('/works/{work}/deny', [WorkController::class, 'deny']);
-Route::get('/works/{work_id}/accept', [WorkController::class, 'accept']);
-Route::get('/works/{work_id}/start', [WorkController::class, 'start']);
-Route::get('/works/{work}/confirm', [WorkController::class, 'confirm']);
-Route::post('/works/{work}/stats', [WorkController::class, 'stats']);
+    Route::post('/works', [WorkController::class, 'store']);
+    Route::get('/works/{work}/deny', [WorkController::class, 'deny']);
+    Route::get('/works/{work_id}/accept', [WorkController::class, 'accept']);
+    Route::get('/works/{work_id}/start', [WorkController::class, 'start']);
+    Route::get('/works/{work}/confirm', [WorkController::class, 'confirm']);
+    Route::post('/works/{work}/stats', [WorkController::class, 'stats']);
 
-Route::get('/payments', [PaymentController::class, 'index']);
+    Route::get('/payments', [PaymentController::class, 'index']);
 
-Route::get('/referrals', [ReferralController::class, 'index']);
-Route::get('/referrals/export', [ReferralController::class, 'export']);
+    Route::get('/referrals', [ReferralController::class, 'index']);
+    Route::get('/referrals/export', [ReferralController::class, 'export']);
+});
+
