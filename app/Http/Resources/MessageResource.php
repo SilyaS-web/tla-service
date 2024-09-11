6d8e29af5112137e84c4c4f36f20c17333d6ea9f
Алексей Андреев <andreev@htmlacademy.pro>
2024-09-11 21:47:11 +0400

    Добавлен юзер айди в message ресурс

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -16,6 +16,7 @@ class MessageResource extends JsonResource
         return [
             'id' => $this->id,
             'message' => $this->message,
+            'user_id' => $this->user_id,
             'files' => MessageFileResource::collection($this->messageFiles),
             'viewed_at' => isset($this->viewed_at) ? $this->viewed_at->format('Y-m-d H:i') : null,
             'created_at' => isset($this->created_at) ? $this->created_at->format('Y-m-d H:i') : null,
