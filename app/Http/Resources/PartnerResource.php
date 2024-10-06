85c43d90c8d6d223c98763dc1ceeab90f7b533a5
Алексей Андреев <andreev@htmlacademy.pro>
2024-10-06 20:23:29 +0400

    Добавляет роуты для управления партнёрами api

---
+++ app/Http/Resources/PartnerResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PartnerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($response)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'code' => $this->code,
+            'link' => $this->link,
+        ];
+    }
+}
