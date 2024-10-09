501c29ff7de75e3bb5e5ed950f3f24652ea478fd
Alexey Andreev <lehagagog@mail.ru>
2024-10-09 22:39:08 +0400

    Сохраняет соотношение сторон изображения

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -15,16 +15,16 @@ class ImageService
 
         $image_1x = Image::make($image->getRealPath());
         $image_1x->encode($format);
-        $urls[] = self::saveCompressedImage($image_1x, $folder . '1x/');
+        $urls[] = self::saveImage($image_1x, $folder . '1x/');
 
-        $image_2x = self::compressImage($image, 440, 540, $format);
-        $urls[] = self::saveCompressedImage($image_2x, $folder . '2x/');
+        $image_2x = self::compressImage($image, -1, 540, $format);
+        $urls[] = self::saveImage($image_2x, $folder . '2x/');
 
         return $urls;
     }
 
 
-    public static function saveCompressedImage(\Intervention\Image\Image $image, $folder = ''): string
+    public static function saveImage(\Intervention\Image\Image $image, $folder = ''): string
     {
         $imageName = time() . '.' . 'webp';
         Storage::put('public/' . $folder . $imageName, $image->encode(), 'public');
@@ -34,6 +34,18 @@ class ImageService
     public static function compressImage(UploadedFile $image, int $height = 400, int $width = 400, string $format = 'webp'): \Intervention\Image\Image
     {
         $img = Image::make($image->getRealPath());
+
+        $old_height = $img->height();
+        $old_width = $img->width();
+
+        $k = $old_height / $old_width;
+
+        if ($height === -1) {
+            $height = $width * $k;
+        } else if ($width === -1) {
+            $width = $height / $k;
+        }
+
         $img->resize($height, $width);
         $img->encode($format);
 
