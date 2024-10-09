ddf7295e660d6a2dd5b5b8c6f2782197ef01af89
Alexey Andreev <lehagagog@mail.ru>
2024-10-09 22:14:03 +0400

    Добавляет сжатие изображений проектов и пользователей

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -26,7 +26,7 @@ class ImageService
     {
         $imageName = time() . '.' . 'webp';
         $image_path = $folder . $imageName;
-
+       dd( $image_path);
         Storage::put($folder . $imageName, $image->encode());
         return $image_path;
     }
