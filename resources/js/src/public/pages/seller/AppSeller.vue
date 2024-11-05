3d2cf29c07b0cdfabf81f8b95e4edbe589720d48
Ilya Sofronov <ilya.sofron@mail.ru>
2024-11-06 00:00:30 +0400

    вернул возможность выбирать несколько форматов рекламы

--- resources/js/src/public/pages/seller/AppSeller.vue
+++ resources/js/src/public/pages/seller/AppSeller.vue
@@ -165,7 +165,7 @@ export default{
 
             this.isChatTab = tab === 'chat';
 
-            $('.wrapper').toggleClass('footer_disabled', this.isChatTab)
+            $('.wrapper').toggleClass('footer_disabled', this.isChatTab || tab === 'create_project')
 
             switch (tab){
                 case 'dashboard':
