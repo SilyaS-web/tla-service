0cd9272ef58e9272cbfa0fca9353eadd1926bee8
Alexey Andreev <lehagagog@mail.ru>
2024-11-11 11:55:18 +0400

    Выдача тарифа пользователям без товаров

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -33,7 +33,6 @@ class UserController extends Controller
         if ($user->role == 'seller' && $user->status == 1) {
             if(count($user->projects) === 0) {
                 $tariffs = $user->getActiveTariffs();
-                dd(empty($tariffs));
                 if(count($tariffs) == 0) {
                     $tariff = Tariff::find(19);
                     SellerTariff::create([
