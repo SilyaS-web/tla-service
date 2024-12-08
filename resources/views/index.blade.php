86a98a23874ce6a94107cac05700c9d6ccbf14e5
Ilya Sofronov <ilya.sofron@mail.ru>
2024-12-08 21:42:07 +0400

    фавиконка

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Unbounded:wght@200..900&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="img/adswap_1_2.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="img/adswap_1_2.ico" type="image/x-icon">
 
     <link rel="stylesheet" href="{{ asset('libs/owl/owl.carousel.min.css')  }}">
     <link rel="stylesheet" href="{{ asset('libs/owl/owl.theme.default.css')  }}">
