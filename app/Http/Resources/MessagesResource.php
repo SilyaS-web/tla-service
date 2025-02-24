4aa8bbbe024ef9bc092f132efa3dade819143db7
Alexey Andreev <lehagagog@mail.ru>
2025-02-24 22:11:20 +0400

    Возвращает тз первыйм сообщением

--- app/Http/Resources/MessagesResource.php
+++ app/Http/Resources/MessagesResource.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Message;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class MessagesResource extends JsonResource
+class MessagesResource extends ResourceCollection
 {
     /**
      * Transform the resource into an array.
