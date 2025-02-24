4a51176758de7b27b1b206a0727bbd79100c6945
Alexey Andreev <lehagagog@mail.ru>
2025-02-24 21:38:18 +0400

    Возвращает тз первыйм сообщением

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -275,7 +275,7 @@ class UserController extends Controller
         ];
 
         $messages = MessageResource::collection($messages->get());
-        $merged = $messages->merge([$specification_message]);
+        $merged = $messages->merge($specification_message);
 
         $data = [
             'messages' => $merged,
