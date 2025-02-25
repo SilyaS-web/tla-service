4cac6716a05eb0d98c02ac1ca8557fe72da62709
Alexey Andreev <lehagagog@mail.ru>
2025-02-25 20:38:34 +0400

    Заменяет пробелы в названии

--- app/Http/Controllers/API/WorkController.php
+++ app/Http/Controllers/API/WorkController.php
@@ -121,7 +121,7 @@ class WorkController extends Controller
 
         $work_files = $request->file('files');
         foreach ($work_files as $file) {
-            $file_path = $file->storeAs('works', $project->product_name . '_' . Str::random(10),'public');
+            $file_path = $file->storeAs('works', str_replace(' ', '_', $project->product_name) . '_' . Carbon::now() . '_' . Str::random(10),'public');
 
             foreach ($works as $work) {
                 WorkFile::create([
