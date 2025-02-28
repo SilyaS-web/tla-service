b403f1d99def1bc3a4f46b973c202751784cd2c9
Alexey Andreev <lehagagog@mail.ru>
2025-03-01 00:52:09 +0400

    Статистика для админа

---
+++ app/Http/Requests/Admin/StatsRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class StatsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'start' => 'nullable|date',
+            'end' => 'nullable|date|after:start',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'start.date' => 'Поле "Начальная дата" должно быть корректной датой.',
+            'end.date' => 'Поле "Конечная дата" должно быть корректной датой.',
+            'end.after' => 'Поле "Конечная дата" должно быть позже, чем "Начальная дата".',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'start' => 'Начальная дата',
+            'end' => 'Конечная дата',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  Validator  $validator
+     * @return void
+     *
+     * @throws ValidationException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new ValidationException($validator, response()->json([
+            'message' => 'The given data was invalid.',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
