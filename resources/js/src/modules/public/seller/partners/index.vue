2ffc151a9c70232af32fc0e66ea06a67b67f60ac
Ilya Sofronov <ilya.sofron@mail.ru>
2025-02-28 11:21:29 +0400

    добавил баннер в партнерах. включил партнеров в навигации

--- resources/js/src/modules/public/seller/partners/index.vue
+++ resources/js/src/modules/public/seller/partners/index.vue
@@ -9,119 +9,24 @@
                 </div>
                 <div class="partners__body">
                     <div class="partners__items">
-                        <div class="partners__item-image" style="margin-bottom: -25px">
-                        </div>
-                        <div class="partners__item">
-                            <div class="partners__item-header">
-                                <div class="partners__item-logo">
-                                    <img src="/img/partners/seller24-logo.png" alt="">
-                                </div>
-                                <div class="partners__item-link">
-                                    <a href="https://seller24.ru/">https://seller24.ru/</a>
-                                </div>
-                            </div>
+                        <a href="https://t.me/adswap_blogger/103" class="partners__item-image partners__item-image--width" style="margin-bottom: -25px; background-image: url('/img/partners/Frame 12.png');">
+                        </a>
+                        <div class="partners__item partners__item--fw partners__item--mobile-only">
                             <div class="partners__item-body">
-                                <div class="partners__item-title">
-                                    Зарабатывайте больше на маркетплейсах
+                                <div class="partners__item-title partners__item--accent">
+                                    Конкурс на лучший рилс
                                 </div>
                                 <div class="partners__item-text">
-                                    Считайте прибыль по каждому товару с учетом рекламы и unit-экономику в два клика корректируйте
+                                    Запишите рилс с образом и упоминание сервиса Adswap(или его мини-ап в телеграм), пришлите ссылку в комментарии к конкурсному посту в нашем телерам канале и получите возможность выиграть 10 000 рублей. Голосованием нашей команды мы выберем лучший рилс. Переходите по кнопке подробнее, чтобы получить все условия.<br>
+                                    <b>Итоги подведем 20 марта.</b>
+                                </div>
+                                <div class="partners__item-subtitle partners__item--accent">
+                                    Призовой фонд 10 000 руб.
                                 </div>
                             </div>
                             <div class="partners__item-footer">
-                                <div class="partners__item-promo">
-                                    <p>Промокод 10%</p>
-                                    <p>Другие предложения</p>
-                                </div>
                                 <div class="partners__item-btns">
-                                    <a href="https://seller24.ru/" class="btn btn-secondary">
-                                        Подробнее
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="partners__item">
-                            <div class="partners__item-header">
-                                <div class="partners__item-logo">
-                                    <img src="/img/partners/seller24-logo.png" alt="">
-                                </div>
-                                <div class="partners__item-link">
-                                    <a href="https://seller24.ru/">https://seller24.ru/</a>
-                                </div>
-                            </div>
-                            <div class="partners__item-body">
-                                <div class="partners__item-title">
-                                    Зарабатывайте больше на маркетплейсах
-                                </div>
-                                <div class="partners__item-text">
-                                    Считайте прибыль по каждому товару с учетом рекламы и unit-экономику в два клика корректируйте
-                                </div>
-                            </div>
-                            <div class="partners__item-footer">
-                                <div class="partners__item-promo">
-                                    <p>Промокод 10%</p>
-                                    <p>Другие предложения</p>
-                                </div>
-                                <div class="partners__item-btns">
-                                    <a href="https://seller24.ru/" class="btn btn-secondary">
-                                        Подробнее
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="partners__item">
-                            <div class="partners__item-header">
-                                <div class="partners__item-logo">
-                                    <img src="/img/partners/seller24-logo.png" alt="">
-                                </div>
-                                <div class="partners__item-link">
-                                    <a href="https://seller24.ru/">https://seller24.ru/</a>
-                                </div>
-                            </div>
-                            <div class="partners__item-body">
-                                <div class="partners__item-title">
-                                    Зарабатывайте больше на маркетплейсах
-                                </div>
-                                <div class="partners__item-text">
-                                    Считайте прибыль по каждому товару с учетом рекламы и unit-экономику в два клика корректируйте
-                                </div>
-                            </div>
-                            <div class="partners__item-footer">
-                                <div class="partners__item-promo">
-                                    <p>Промокод 10%</p>
-                                    <p>Другие предложения</p>
-                                </div>
-                                <div class="partners__item-btns">
-                                    <a href="https://seller24.ru/" class="btn btn-secondary">
-                                        Подробнее
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="partners__item">
-                            <div class="partners__item-header">
-                                <div class="partners__item-logo">
-                                    <img src="/img/partners/seller24-logo.png" alt="">
-                                </div>
-                                <div class="partners__item-link">
-                                    <a href="https://seller24.ru/">https://seller24.ru/</a>
-                                </div>
-                            </div>
-                            <div class="partners__item-body">
-                                <div class="partners__item-title">
-                                    Зарабатывайте больше на маркетплейсах
-                                </div>
-                                <div class="partners__item-text">
-                                    Считайте прибыль по каждому товару с учетом рекламы и unit-экономику в два клика корректируйте
-                                </div>
-                            </div>
-                            <div class="partners__item-footer">
-                                <div class="partners__item-promo">
-                                    <p>Промокод 10%</p>
-                                    <p>Другие предложения</p>
-                                </div>
-                                <div class="partners__item-btns">
-                                    <a href="https://seller24.ru/" class="btn btn-secondary">
+                                    <a href="https://t.me/adswap_blogger/103" class="btn btn-primary">
                                         Подробнее
                                     </a>
                                 </div>
@@ -133,3 +38,5 @@
         </div>
     </div>
 </template>
+<script setup lang="ts">
+</script>
