634f63eab4d366e7245e6af471561bfa0f761f1f
Ilya Sofronov <ilya.sofron@mail.ru>
2025-03-26 19:21:24 +0400

    поправил верстку в моих проектахз

--- resources/js/src/modules/public/seller/my-projects/index.vue
+++ resources/js/src/modules/public/seller/my-projects/index.vue
@@ -8,8 +8,11 @@
                 <div class="list-projects__title title">
                     Список моих проектов
                 </div>
-                <div class="" style="display: flex; gap: 10px; flex-wrap: wrap;">
-                    <button class="btn btn-primary projects-list__filter-btn">Фильтры</button>
+                <div class="list-header__btns" style="display: flex; gap: 10px; flex-wrap: wrap;">
+                    <button class="btn btn-primary projects-list__filter-btn">
+                        <svg width="" height="" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 10.5A3.502 3.502 0 0 0 18.355 8H21a1 1 0 1 0 0-2h-2.645a3.502 3.502 0 0 0-6.71 0H3a1 1 0 0 0 0 2h8.645A3.502 3.502 0 0 0 15 10.5zM3 16a1 1 0 1 0 0 2h2.145a3.502 3.502 0 0 0 6.71 0H21a1 1 0 1 0 0-2h-9.145a3.502 3.502 0 0 0-6.71 0H3z" fill=""/></svg>
+                        Фильтры
+                    </button>
                 </div>
             </div>
             <div class="profile-projects__sort form-group" style="margin-bottom:32px;">
