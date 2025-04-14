e3aa941614cc07906f948657f900bf496d6e132d
Ilya Sofronov <ilya.sofron@mail.ru>
2025-04-14 21:41:59 +0400

    исправил змейку

--- resources/views/snake_game/index.blade.php
+++ resources/views/snake_game/index.blade.php
@@ -1324,7 +1324,7 @@ App
 
     g.time = new g.Time();
 
-    const chatID = getParamFromUrl('chat_id');
+    const chatID = getUrlParameter('chat_id');
 
     if(!chatID){
         throw new Error('Отсутствует айди чата')
@@ -1456,24 +1456,19 @@ Utils
 
 ================================================*/
 
-function getParamFromUrl(query) {
-    var vars = query.split("&");
-    var query_string = {};
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        var key = decodeURIComponent(pair.shift());
-        var value = decodeURIComponent(pair.join("="));
-        if (typeof query_string[key] === "undefined") {
-            query_string[key] = value;
-        } else if (typeof query_string[key] === "string") {
-            var arr = [query_string[key], value];
-            query_string[key] = arr;
-        } else {
-            query_string[key].push(value);
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
         }
     }
-    return query_string;
-}
+    return false;
+};
 
 /*================================================
 
