0f2b194180a4da42d5eeef7ab7bc6fe338edcdd4
Ilya Sofronov <ilya.sofron@mail.ru>
2025-04-16 21:59:50 +0400

    исправил мобилку змейки

--- resources/views/snake_game/index.blade.php
+++ resources/views/snake_game/index.blade.php
@@ -1314,10 +1314,6 @@ function setTelegramWindow(ms) {
 
         await window
           .TelegramWebviewProxy
-          .postEvent('web_app_setup_swipe_behavior', web_app_setup_swipe_behavior)
-
-        await window
-          .TelegramWebviewProxy
           .postEvent('web_app_request_fullscreen')
 
         resolve()
