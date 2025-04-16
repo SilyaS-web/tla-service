cf30f3becbb76fd842c87df597f0996fe717e10b
Ilya Sofronov <ilya.sofron@mail.ru>
2025-04-16 19:27:23 +0400

    исправил мобилку змейки

--- resources/views/snake_game/index.blade.php
+++ resources/views/snake_game/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Змейка</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/snake.css">
+    <link rel="stylesheet" href="/css/snake.css?v=1">
 </head>
 <body>
     <div class="wrapper">
